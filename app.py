a6a1c0b618624bbd9f5c79695ef11c3532bfb63c
Reeva <reeva@Andriens-Mac-mini.local>
2025-08-24 02:26:28 +0100

    Random cocktail feature added

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def get_cocktails(query="margarita"):
     print(data)
     drinks = data.get("drinks", [])
 
+    # if drinks == None:
+    #     return []
+
     # Remove duplicates
     unique_drinks = []
     seen_names = set()
@@ -47,24 +50,65 @@ def get_cocktails(query="margarita"):
                 
     return full_cocktail_list
 
+# FUNCTION TO GET RANDOM COCKTAIL
+def get_random_cocktail():
+    url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    response = requests.get(url)
+    data = response.json()
+    drinks = data.get("drinks", [])
+
+    if not drinks:
+        return None 
+    
+    drink = drinks[0]
+
+    ingredients = []
+    for i in range(1, 16):
+                ingredient = drink.get(f"strIngredient{i}")
+                measure = drink.get(f"strMeasure{i}")
+                if ingredient and ingredient.strip():
+                    ingredients.append(
+                        f"{measure.strip() if measure else ''} {ingredient.strip()}".strip()
+                        ) 
+    return {
+        "name": drink.get("strDrink"),
+        "image": drink.get("strDrinkThumb"), 
+        "instructions": drink.get("strInstructions"),
+        "ingredients": ingredients
+    }
 
 
+# RANDOM ROUTE
+# @app.route('/random')
+# def random_cocktail():
+#     cocktail = get_random_cocktail()
+#     return render_template('index.html', cocktail=cocktail)
 
-# HOMEPAGE
+# HOMEPAGE ROUTE
 @app.route('/', methods=["GET", "POST"])
 def home():
     cocktails = []
     message = None
 
     if request.method == "POST":
-        search_query = request.form.get("cocktail_name" or "").strip()
+        action = request.form.get("action")
 
-        if not search_query:
-            message = "Please enter cocktail name."
-        else:
-            cocktails = get_cocktails(search_query)
-            if not cocktails:
-                message = "No results found, please try again"
+        if action == "search":
+            search_query = request.form.get("cocktail_name" or "").strip()
+
+            if not search_query:
+                message = "Please enter cocktail name."
+            else:
+                cocktails = get_cocktails(search_query)
+                if not cocktails:
+                    message = f"No results found for'{search_query}'."
+
+        elif action == "random":
+            cocktail = get_random_cocktail()
+            if cocktail:
+                cocktails = [cocktail]
+            else:
+                message = "No random cocktail found."
 
         return render_template('index.html', cocktails=cocktails, message=message)
         # ELSE STATEMENT FOR GET
